cda4fbaef5f0268700e81d36bea0b53a0866124f
Aman Singh <amankumarsingh1412@gmail.com>
2025-07-05 18:14:18 +0530

    avalon: Derp-ify

--- device.mk
+++ device.mk
@@ -31,6 +31,7 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
--- lineage_avalon.mk
+++ lineage_avalon.mk
@@ -9,6 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from avalon device
 $(call inherit-product, device/oneplus/avalon/device.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+DERPFEST_BUILD_TYPE := Official
+TARGET_USES_BLUR := true
+TARGET_DISABLE_EPPE := true
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
